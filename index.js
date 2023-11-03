3857b121a02b84e723f4479eacc509dad69d11a9
Ayush Ray <rayayush@gmail.com>
2023-11-03 07:35:44 +0530

    Improving gamje logic

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-let card1 = 6
-let card2 = 8
-let card3 = 7
-let message;
-let sum = card1 + card2 + card3
-let result = document.getElementById("msg")
-let ans = document.getElementById("sum")
-let cards = document.getElementById("crds")
 
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+console.log(cards)
+
+//for generating random Card
+function getRendomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) +1
+    if(randomNumber >10){
+        return 10
+    }else if(randomNumber === 1){
+        return 11
+    }else{
+        return randomNumber
+    }
+}
+
+//for starting the game
 function startGame(){
+    isAlive = true
+    let firstCard = getRendomCard();
+    let secondCard = getRendomCard();
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame();
+}
+
+function renderGame(){
+    cardsEl.textContent = "cards: "
+    for(let i=0;i<cards.length;i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "sum: " + sum
     if (sum<=20){
         message = "Pick another card?"
     }else if(sum === 21){
         message = "Congratulation you won!"
+        hasBlackJack = true
     }else{
         message = "You lose"
+        isAlive = false
     }
-    result.innerText = message
-    ans.innerText += sum
-    cards.innerText += card1 + ",  " + card2 + ", " + card3
-
+    messageEl.textContent = message
 }
 
 function newCard(){
-    result.innerText = "Adding one new crd to the deck!"
-    startGame();
+    let card = getRendomCard();
+    sum += card
+    cards.push(card)
+    renderGame();
 }
 
 
